a63723a527983f05521f9af532791a233ae48edb
glenchiridza <glenchiridz@gmail.com>
2022-02-22 12:15:07 +0200

    service sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { EventListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/events.service';
 import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
@@ -16,7 +17,9 @@ import { NavbarComponent } from './nav/navbar.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    EventService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
