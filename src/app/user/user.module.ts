d44b59d59998f5f299528a102d4284e9fbcce238
glenchiridza <glenchiridz@gmail.com>
2022-02-28 23:03:27 +0200

    reactive form validators

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -4,7 +4,7 @@ import { RouterModule } from "@angular/router";
 import { ProfileComponent } from "./profile.component";
 import {userRoutes} from './user.routes' 
 import { LoginComponent } from "./login.component";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
     declarations:[
@@ -14,6 +14,7 @@ import { FormsModule } from "@angular/forms";
     imports:[
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterModule.forChild(userRoutes)
     ],
     providers:[
